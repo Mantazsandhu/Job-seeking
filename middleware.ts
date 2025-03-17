ff6dc804f058a3f8b0129c54b803eb771c507f01
Mantazsandhu <mantazs946@gmail.com>
2025-03-17 17:06:28 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,4 +2,7 @@ export { auth as middleware } from "./auth";
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  unstable_allowDynamic: [
+    "**/node_modules/@prisma/client*/**/*.mjs*",
+  ],
 };
